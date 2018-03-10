fa497f7808c05525986efb4cd0ba597bc07342df
Luis Pineda JR <lspnd00@gmail.com>
2018-03-10 16:22:28 -0500

    adding fish to state

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,18 @@ import Inventory from './Inventory';
 
 
 class App extends React.Component{
+  state = {
+        fishes: {},
+        order: {}
+  };
+  addFish = fish => {
+        // 1. take a copy of existing state
+        const fishes = { ...this.state.fishes };
+        // 2. add our new to fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // 3. set the new fishes object to state
+        this.setState({ fishes });
+  };
   render(){
     return(
       <div className="catch-of-the-day">
@@ -12,7 +24,7 @@ class App extends React.Component{
       <Header tagline="Fresh Seafood Market" />
       </div>
       <Order />
-      <Inventory />
+      <Inventory addFish={this.addFish} />
       </div>
     );
   }
